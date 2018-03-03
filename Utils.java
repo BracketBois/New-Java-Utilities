293cb0ee63d52ec25cfe778d6dc6c9a1b0374abc
nwarsalee <nwars1@ocdsb.ca>
2018-03-03 18:51:19 -0500

    Utilities
    This is the first build of the utilities class. The build includes...
    * The class itself with comments about the project
    * A class method called roundToX that will round a number (type double) to the xth decimal place

---
+++ Utils.java
@@ -0,0 +1,20 @@
+/* A Utilities class designed and created by Bracket Bois (Eren, Denzel and Nabeel)
+ * March 3rd 2018
+ * Build Version 1.0
+ */
+
+class Utils {
+  
+  public static double roundToX (double number, int x) {
+    double rounded;
+    // If the second parameter is negative or zero, just returns the number
+    if (x<0) {
+      return number;
+    }
+    // Multiplies the number by 10 to the power of how many decimals they want, then rounds that number
+    rounded = Math.round(number*(Math.pow(10, x))); 
+    rounded /= Math.pow(10, x); // Divides the number by the intial 10^x it was multiplied by before
+    return rounded; // returns the rounded number
+  }
+
+}
